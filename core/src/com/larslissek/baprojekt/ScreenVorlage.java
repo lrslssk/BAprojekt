48780cea068524340f66ca4c37095bde91c7a36c
Lars <lars.lissek@tu-dortmund.de>
2017-06-07 11:24:46 +0200

    ScreenVorlage Class

---
+++ core/src/com/larslissek/baprojekt/ScreenVorlage.java
@@ -0,0 +1,87 @@
+package com.larslissek.baprojekt;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+public class ScreenVorlage implements Screen {
+
+	
+	MyGdxGame game;
+	OrthographicCamera cam;
+	StretchViewport port;
+	SpriteBatch batch;
+	
+	
+	FreeTypeFontGenerator generator;
+	
+	public ScreenVorlage(MyGdxGame game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void show() {
+		cam = new OrthographicCamera();
+		port = new StretchViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, cam);
+		cam.position.set(port.getWorldWidth() / 2, port.getWorldHeight() / 2, 0);
+		
+		
+		batch = new SpriteBatch();
+		
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("ui/ocraextended.ttf"));
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = 29;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		update(delta);
+		
+		
+		batch.setProjectionMatrix(cam.combined);
+		
+		batch.begin();
+		batch.end();
+		
+		
+		Gdx.graphics.setTitle("BAprojekt | " + Gdx.graphics.getFramesPerSecond() + " FPS");
+	}
+	
+	private void update(float delta) {
+		cam.update();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		port.update(width, height);
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
